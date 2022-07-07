eb35e135a28e8cf3a6613f829299dc32960ce1a0
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-07 10:04:37 +0900

    user service

---
+++ src/user/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { User } from "./user.entity";
+import { CreateUserDto } from "./dto/create-user.dto";
+
+@Injectable()
+export class UserService {
+  private items: User[] = [];
+  private seq = 1;
+
+  findAll(): User[] { return this.items; }
+
+  findOne(id: number): User {
+    const found = this.items.find((x) => x.id === id);
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  create(dto: CreateUserDto): User {
+    const item = { id: this.seq++, ...dto };
+    this.items.push(item);
+    return item;
+  }
+}
