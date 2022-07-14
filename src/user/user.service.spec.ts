e11fb55c6aa324d6801f3f4a0b8e0349f6a08365
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-14 14:32:46 +0900

    user spec

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,8 @@
+import { UserService } from "./user.service";
+
+describe("UserService", () => {
+  it("creates", () => {
+    const s = new UserService();
+    expect(s.create({} as any).id).toBe(1);
+  });
+});
