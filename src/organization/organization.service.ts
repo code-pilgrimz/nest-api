4371075e56dc02e055a53d786467a75d12575592
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-18 11:05:14 +0900

    organization service

---
+++ src/organization/organization.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Organization } from "./organization.entity";
+import { CreateOrganizationDto } from "./dto/create-organization.dto";
+
+@Injectable()
+export class OrganizationService {
+  private items: Organization[] = [];
+  private seq = 1;
+
+  findAll(): Organization[] { return this.items; }
+
+  findOne(id: number): Organization {
+    const found = this.items.find((x) => x.id === id);
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  create(dto: CreateOrganizationDto): Organization {
+    const item = { id: this.seq++, ...dto };
+    this.items.push(item);
+    return item;
+  }
+}
