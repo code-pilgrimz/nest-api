bd67b66c8194e2dec643807b5f41bb11861c1b9a
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-20 17:43:03 +0900

    organization controller

---
+++ src/organization/organization.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from "@nestjs/common";
+import { OrganizationService } from "./organization.service";
+import { CreateOrganizationDto } from "./dto/create-organization.dto";
+
+@Controller("organizations")
+export class OrganizationController {
+  constructor(private readonly service: OrganizationService) {}
+
+  @Get()
+  findAll() { return this.service.findAll(); }
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) { return this.service.findOne(id); }
+
+  @Post()
+  create(@Body() dto: CreateOrganizationDto) { return this.service.create(dto); }
+}
