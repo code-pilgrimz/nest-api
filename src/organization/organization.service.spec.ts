bbda2cd5c7a56f1b4716c9987e430fc6bf89455f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-26 18:25:11 +0900

    organization spec

---
+++ src/organization/organization.service.spec.ts
@@ -0,0 +1,8 @@
+import { OrganizationService } from "./organization.service";
+
+describe("OrganizationService", () => {
+  it("creates", () => {
+    const s = new OrganizationService();
+    expect(s.create({} as any).id).toBe(1);
+  });
+});
