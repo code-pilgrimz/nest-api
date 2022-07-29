9a133ff2f86b748ef4b2076d66072ac517db8860
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-07-30 00:49:41 +0900

    project module

---
+++ src/project/project.module.ts
@@ -0,0 +1,6 @@
+import { Module } from "@nestjs/common";
+import { ProjectController } from "./project.controller";
+import { ProjectService } from "./project.service";
+
+@Module({ controllers: [ProjectController], providers: [ProjectService] })
+export class ProjectModule {}
