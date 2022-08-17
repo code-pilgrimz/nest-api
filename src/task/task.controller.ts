84bd2a3f9a4fd0619f9025b173f47267aebd0349
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-08-17 16:43:45 +0900

    task controller

---
+++ src/task/task.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from "@nestjs/common";
+import { TaskService } from "./task.service";
+import { CreateTaskDto } from "./dto/create-task.dto";
+
+@Controller("tasks")
+export class TaskController {
+  constructor(private readonly service: TaskService) {}
+
+  @Get()
+  findAll() { return this.service.findAll(); }
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) { return this.service.findOne(id); }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) { return this.service.create(dto); }
+}
