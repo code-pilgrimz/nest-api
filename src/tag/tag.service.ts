f88acdb1e002c0617b822cd5acd0881c3007bf0c
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-09-27 11:44:50 +0900

    tag service

---
+++ src/tag/tag.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Tag } from "./tag.entity";
+import { CreateTagDto } from "./dto/create-tag.dto";
+
+@Injectable()
+export class TagService {
+  private items: Tag[] = [];
+  private seq = 1;
+
+  findAll(): Tag[] { return this.items; }
+
+  findOne(id: number): Tag {
+    const found = this.items.find((x) => x.id === id);
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  create(dto: CreateTagDto): Tag {
+    const item = { id: this.seq++, ...dto };
+    this.items.push(item);
+    return item;
+  }
+}
