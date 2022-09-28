596f0d87b5efcef7f1ea8ab61084750816b324de
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-09-28 17:28:21 +0900

    tag controller

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from "@nestjs/common";
+import { TagService } from "./tag.service";
+import { CreateTagDto } from "./dto/create-tag.dto";
+
+@Controller("tags")
+export class TagController {
+  constructor(private readonly service: TagService) {}
+
+  @Get()
+  findAll() { return this.service.findAll(); }
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) { return this.service.findOne(id); }
+
+  @Post()
+  create(@Body() dto: CreateTagDto) { return this.service.create(dto); }
+}
