4a53ee59f930b0089a148ef54c1a2df516496b3e
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-09-30 17:03:03 +0900

    tag spec

---
+++ src/tag/tag.service.spec.ts
@@ -0,0 +1,8 @@
+import { TagService } from "./tag.service";
+
+describe("TagService", () => {
+  it("creates", () => {
+    const s = new TagService();
+    expect(s.create({} as any).id).toBe(1);
+  });
+});
