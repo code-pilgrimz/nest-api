403906bdc8dc1ad1ea29db9fb9b8c7a670af9c7d
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-11-10 18:12:25 +0900

    payment spec

---
+++ src/payment/payment.service.spec.ts
@@ -0,0 +1,8 @@
+import { PaymentService } from "./payment.service";
+
+describe("PaymentService", () => {
+  it("creates", () => {
+    const s = new PaymentService();
+    expect(s.create({} as any).id).toBe(1);
+  });
+});
