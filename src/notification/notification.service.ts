681ba5ae4030d0bd071d7caf6513893cfbedd3e8
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-11-18 11:33:32 +0900

    notification service

---
+++ src/notification/notification.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Notification } from "./notification.entity";
+import { CreateNotificationDto } from "./dto/create-notification.dto";
+
+@Injectable()
+export class NotificationService {
+  private items: Notification[] = [];
+  private seq = 1;
+
+  findAll(): Notification[] { return this.items; }
+
+  findOne(id: number): Notification {
+    const found = this.items.find((x) => x.id === id);
+    if (!found) throw new NotFoundException();
+    return found;
+  }
+
+  create(dto: CreateNotificationDto): Notification {
+    const item = { id: this.seq++, ...dto };
+    this.items.push(item);
+    return item;
+  }
+}
