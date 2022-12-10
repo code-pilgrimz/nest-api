f786ef2706218609e66defdfa88b13419163fd6f
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-12-10 13:17:04 +0900

    webhook spec

---
+++ src/webhook/webhook.service.spec.ts
@@ -0,0 +1,8 @@
+import { WebhookService } from "./webhook.service";
+
+describe("WebhookService", () => {
+  it("creates", () => {
+    const s = new WebhookService();
+    expect(s.create({} as any).id).toBe(1);
+  });
+});
