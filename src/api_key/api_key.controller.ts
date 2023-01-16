5f41d79c0cc5855fc99fbb51e1d1a8856ed47902
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2023-01-16 17:11:09 +0900

    api_key controller

---
+++ src/api_key/api_key.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Body, Param, ParseIntPipe } from "@nestjs/common";
+import { ApiKeyService } from "./api_key.service";
+import { CreateApiKeyDto } from "./dto/create-api_key.dto";
+
+@Controller("api_keys")
+export class ApiKeyController {
+  constructor(private readonly service: ApiKeyService) {}
+
+  @Get()
+  findAll() { return this.service.findAll(); }
+
+  @Get(":id")
+  findOne(@Param("id", ParseIntPipe) id: number) { return this.service.findOne(id); }
+
+  @Post()
+  create(@Body() dto: CreateApiKeyDto) { return this.service.create(dto); }
+}
